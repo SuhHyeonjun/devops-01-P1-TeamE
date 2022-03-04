c85c77a8721dace4cc3129f9f8430864afeb9589
passingbreeze <jungmin1237@pusan.ac.kr>
2022-03-04 16:20:38 +0900

    merge and fix

--- server/server-express.js
+++ server/server-express.js
@@ -20,11 +20,13 @@ const choiceRouter = require(`../routes/choice`);
 const dbRouter = require(`../db_connect/dbconn`);
 
 app.use(`/`, dbRouter);
-app.use(`/voted_result`, votedResult); // http://localhost:4000/voted_result
-app.use(`/users`, usersRouter); // http://localhost:4000/users
-app.use(`/register`, registerRouter); // http://localhost:4000/register
+app.use(`/voted_result`, votedResultRouter); // http://localhost:4000/voted_result
+app.use(`/vote`, voteRouter);               // http://localhost:4000/vote
+app.use(`/register`, registerRouter);       // http://localhost:4000/register
+app.use(`/quiz`, quizRouter);               // http://localhost:4000/quiz
+app.use(`/choice`, choiceRouter);           // http://localhost:4000/choice
 
-app.get(`/`, (req, res) => { // http://localhost:4000/
+app.get(`/`, (req, res) => { // http://localhost:4000/, 서버 동작 확인
   return res.status(200).send(`<h1><center>Server is running...</h1></center>`)
 });
 
