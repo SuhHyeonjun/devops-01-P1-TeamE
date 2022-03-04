c8ff18d09661de0203325ba52ed8bd026d724bad
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-03-04 13:52:47 +0900

    serve-express.js 수정

--- routes/register.js
+++ routes/register.js
@@ -1,13 +1,25 @@
 const express = require(`express`);
 const router = express.Router();
 // API 명세를 보고 여기부터 코드 작성해주세요.
-router.route(`/`)
+router.route(`/`) // app.use(`/register`)
     .post((request, response) => {
+        console.log(request.body);
         const {username, email, password} = request.body;
-        const inpData = {uid:1};
-        return response
-        .statusCode(201)
-        .send(inpData)
-    })
+        let inpData = {username, email, password};
+        const outData = {"uid" : 1 };
+        inpData = null;
+        if (inpData) {
+            return response
+                .status(201)
+                .header('Content-Type','application/json')
+                .send(outData)
+        } else {
+            return response
+                    .status(404)
+                    .header('Content-Type','application/json')
+                    .send({"error" : "Not Found"})
+        }
+    }
+    )    
 // 여기까지
 module.exports = router;
