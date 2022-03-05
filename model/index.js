232dea191cc4554ffe83a22e74fef89c680ec9a4
passingbreeze <jungmin1237@pusan.ac.kr>
2022-03-05 15:26:36 +0900

    add nodemon package and files to display server status

--- model/index.js
+++ model/index.js
@@ -1,23 +1,31 @@
 const express = require(`express`);
 const router = express.Router();
-const pg = require('pg');
-//or native libpq bindings
-//var pg = require('pg').native
+const mongoose = require('mongoose');
 
-const conString = process.env.DB_URL; //Can be found in the Details page
-let client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
+// 몽구스 연결 함수
+const mongoConnect = () => {
+    if (process.env.NODE_ENV !== 'production') {
+        mongoose.set('debug', true); // 몽고 쿼리가 콘솔에서 뜨게 한다.
     }
-    console.log(result.rows[0].theTime);
-    // >> output: 2018-08-23T14:02:57.117Z
-    client.end();
-  });
+    mongoose.connect(process.env.DB_URL, 
+      (error) => {
+        if (error) {
+          console.log('Connection Error with MongoDB', error);
+        } else {
+          console.log('Success to connect MongoDB');
+        }
+      }
+    )
+};
+
+// 몽구스 커넥션에 이벤트 리스너를 달게 해준다. 에러 발생 시 에러 내용을 기록하고, 연결 종료 시 재연결을 시도한다.
+mongoose.connection.on('error', (error) => {
+  console.error('몽고디비 연결 에러', error);
 });
 
-module.exports = router;
+mongoose.connection.on('disconnected', () => {
+  console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
+  mongoConnect(); // 연결 재시도
+});
+
+module.exports = mongoConnect;
--- server/server-express.js
+++ server/server-express.js
@@ -17,9 +17,9 @@ const voteRouter = require(`../routes/vote`);
 const registerRouter = require(`../routes/register`);
 const quizRouter = require(`../routes/quiz`);
 const choiceRouter = require(`../routes/choice`);
-const dbRouter = require(`../model`);
+const mongoConnect = require('../model');
 
-app.use(`/`, dbRouter);
+mongoConnect();
 app.use(`/voted_result`, votedResultRouter); // http://localhost:4000/voted_result
 app.use(`/vote`, voteRouter);               // http://localhost:4000/vote
 app.use(`/register`, registerRouter);       // http://localhost:4000/register
@@ -27,17 +27,17 @@ app.use(`/quiz`, quizRouter);               // http://localhost:4000/quiz
 app.use(`/choice`, choiceRouter);           // http://localhost:4000/choice
 
 app.get(`/`, (req, res) => { // http://localhost:4000/, 서버 동작 확인
-  return res.status(200).send(`<h1><center>Server is running...</h1></center>`)
+  try {
+    return res
+      .status(200)
+      .sendFile(`${__dirname}/server_status.html`)
+  }
+  catch(error) {
+    return res
+      .status(500)
+      .sendFile(`${__dirname}/server_error.html`)
+  }
 });
-
-// app.post('/register', (req, res) => {
-//   reply
-//   .code(200)
-//   .header('Content-Type','application/json')
-//   .send('성공')
-// })
-  
-
 
 app.listen(port, ip, () => {
   console.log(`Server listening on http://${ip}:${port}`)
