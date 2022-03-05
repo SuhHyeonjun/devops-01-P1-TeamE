b008b5f0c7b682be02961978cc39053d7cf6d530
passingbreeze <jungmin1237@pusan.ac.kr>
2022-03-05 23:59:39 +0900

    connect DB, complete register and vote API

--- routes/register.js
+++ routes/register.js
@@ -1,27 +1,47 @@
+const jwt = require(`jsonwebtoken`);
+const bcrypt = require(`bcrypt`);
 const express = require(`express`);
+const users = require("../model/users");
 const router = express.Router();
 // API 명세를 보고 여기부터 코드 작성해주세요.
 
 router.route(`/`) // app.use(`/register`)
-    .post((request, response) => {
-        const {username, email, password} = request.body;
-        const inpData = {username, email, password};
-        const outData = {"uid" : 1 };
-        if (inpData) {
+    .post(async (request, response) => {
+        let {username, password} = request.body;
+        const voted_candidate = "";
+        password = bcrypt.hashSync(password, 10);
+        const token = jwt.sign({
+            username : username,
+            password : password
+        }, process.env.SECRET_JWT, {
+            expiresIn: '2h',
+            issuer : `TEAM_E`
+        });
+        // console.log(usertoken);
+        const countUsers = await users.find({});
+        const uid = countUsers.length + 1;
+        const inpData = {uid, username, password, voted_candidate};
+        const registerResult = new users(inpData);
+        // console.log(registerResult);
+        if (registerResult) {
+            await registerResult.save();
             return response
+                .cookie("access_token", token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                  })
                 .status(201)
                 .header('Content-Type','application/json')
-                .send(outData)
+                .json({
+                    "uid" : registerResult.uid,
+                    "token" : "Token Published"
+                })
         } else {
             return response
                     .status(404)
                     .header('Content-Type','application/json')
-                    .send({"error" : "Not Found"})
+                    .send({"error" : "Register Failed"})
         }
-    })    
-
-    .get((request, response) => {
-        return;
-    })
+    })   
 // 여기까지
 module.exports = router;
--- routes/vote.js
+++ routes/vote.js
@@ -1,26 +1,53 @@
+const jwt = require(`jsonwebtoken`);
+const users = require(`../model/users`);
 const express = require(`express`);
 const router = express.Router();
 
 router.route(`/`)
-  
-    .put((request, response) => {
-        const {Voted_candidate : candidate_id} = request.body;
-        const inpData = {Voted_candidate : candidate_id};
-        const outData = {
-            "user_name" : "서현준", 
-            "voted_candidate" : "윤석열"
-        };
-        if (inpData) {
-            return response
-            .status(201)
-            .header('Content-Type','application/json')
-            .send(outData)
-        } else {
-            return response
-            .status(404)
-            .header('Content-Type','application/json')
-            .send({"error" : "Vote Failed"})
+    .put(async (request, response) => {
+        const token = request.cookies.access_token;
+        if(token) {
+            try {
+                const data = jwt.verify(token, process.env.SECRET_JWT);
+                const username = data.username;
+                const updateVoted = request.body.voted_candidate;
+                if(updateVoted > 3){
+                    return response
+                    .status(404)
+                    .header('Content-Type','application/json')
+                    .send({"error" : "Vote Failed"})
+                }
+                const updateResult = await users.findOneAndUpdate(
+                    { "username" : username }, 
+                    {"voted_candidate" : updateVoted },
+                    { returnDocument : `after` }
+                );
+                if (updateResult) {
+                    return response
+                    .status(201)
+                    .header('Content-Type','application/json')
+                    .send({
+                        "username" : updateResult.username,
+                        "voted_candidate" : updateResult.voted_candidate
+                    })
+                } else {
+                    return response
+                    .status(404)
+                    .header('Content-Type','application/json')
+                    .send({"error" : "Vote Failed"})
+                }
+            } catch {
+                return response
+                    .status(403)
+                    .header('Content-Type','application/json')
+                    .send({ "error" : "Authorization Failed" });
+            }
         }
+        else
+            return response
+                .status(403)
+                .header('Content-Type','application/json')
+                .send({ "error" : "Authorization Failed" });
     })
  
     
