1dd87977f8d0df22d79cfc8e29a55c6ef2cae7fd
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-03-06 21:12:35 +0900

    choice 추가

--- routes/quiz.js
+++ routes/quiz.js
@@ -4,22 +4,14 @@ const express = require(`express`);
 const router = express.Router();
 
 router.route(`/`)
-<<<<<<< HEAD
-    .get((request, response) => {
-        let outData = {
-            "question" : "주제",
-            "words" : []
-        };
 
-        if (outData) {
-=======
     .get( async (request, response) => {
      const quiz_id = request.params.qid;
      const questionFindResult = await quiz.find({"quiz_id" : quiz_id})
      const quizFindResult = await words.find({"quiz_id" : quiz_id})
-        if (questionFindResult&quizFindResult)
+        if (questionFindResult&&quizFindResult)
         {
->>>>>>> 4272a7920289b9f066762b51c9f3306df2ec6845
+
             return response
             .status(200)
             .header('Content-Type','application/json')
