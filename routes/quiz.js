ecacfcf5382cc7121abe0c0912c97ecd479e9d43
bigheat0106 <99108763+bigheat0106@users.noreply.github.com>
2022-03-06 21:01:04 +0900

    a
    a
    
    Co-Authored-By: SuhHyeonjun <99153215+SuhHyeonjun@users.noreply.github.com>

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,23 +1,27 @@
+const quiz = require('../model/quiz')
+const words = require('../model/words')
 const express = require(`express`);
 const router = express.Router();
 
 router.route(`/`)
-    .get((request, response) => {
-        let outData = {
-            "questuon" : "주제",
-            "words" : []
-        };
-
-        if (outData) {
+    .get( async (request, response) => {
+     const quiz_id = request.params.qid;
+     const questionFindResult = await quiz.find({"quiz_id" : quiz_id})
+     const quizFindResult = await words.find({"quiz_id" : quiz_id})
+        if (questionFindResult&quizFindResult)
+        {
             return response
             .status(200)
             .header('Content-Type','application/json')
-            .send(outData)
-        } else {
+            .send(questionFindResult&quizFindResult);
+        
+        }
+        else
+        {
             return response
             .status(404)
             .header('Content-Type','application/json')
-            .send({"error" : "Quiz Not Found"})
+            .send({"error" : "Quetion Not Found"})
         }
     })
 
